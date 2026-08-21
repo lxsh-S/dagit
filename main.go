0d6b2071dbaceb4cb1d0c058cae1e4f7bdf48b9d
lxsh-S <lxsh-S>
2026-08-22 01:34:48 +0530

    chore: Remove noise from main.go entry

--- main.go
+++ main.go
@@ -11,27 +11,6 @@ func main() {
 	if git.IsGitRepo() {
 		// We'll begin the TUI here
 		TUI.Main()
-
-		// ---------------------------------------------------------------
-		// fmt.Println("Yes we are in a git repo")
-		// repoName, err := git.GetRepoName()
-		// if err == nil {
-		// 	fmt.Println(repoName)
-		// }
-		// repoURL, err := git.GetRemoteURL()
-		// if err == nil {
-		// 	fmt.Println(repoURL)
-		// }
-		//
-		// repoOwner, err := git.GetRepoOwner()
-		// if err == nil {
-		// 	fmt.Println(repoOwner)
-		// }
-		//
-		// crBranch, err := git.GetCurrentBranch()
-		// if err == nil {
-		// 	fmt.Println(crBranch)
-		// }
 	} else {
 		// If its not a git repo then we dont open our TUI
 		fmt.Println("err: this isn't a git repo")
