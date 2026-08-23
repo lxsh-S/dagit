9aa17880bfe455b3ffeb6e68c8d585e317888be6
lxsh-S <kumarlakshya973@gmail.com>
2026-08-23 18:18:13 +0530

    update: Remove old way to refresh data

--- TUI/update.go
+++ TUI/update.go
@@ -45,37 +45,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tickMsg:
 		m.refreshData()
-		return m, tickEveryWhen(3 * time.Second)
+		return m, tickEveryWhen(1 * time.Second)
 
 	case tea.KeyPressMsg:
 		if msg.String() == "ctrl+c" || msg.String() == "q" {
 			return m, tea.Quit
-		} else if msg.String() == "r" {
-			logs, err := git.GetLog(5)
-			if err == nil {
-				m.logs = logs
-			}
-
-			reponame, err := git.GetRepoName()
-			if err == nil {
-				m.reponame = reponame
-			}
-
-			ownername, err := git.GetRepoOwner()
-			if err == nil {
-				m.ownername = ownername
-			}
-
-			url, err := git.GetRemoteURL()
-			if err == nil {
-				m.url = url
-			}
-
-			branch, err := git.GetCurrentBranch()
-			if err == nil {
-				m.currentbranch = branch
-			}
-			return m, tea.ClearScreen
 		}
 	}
 	return m, nil
